3cfbfc48f9efb57c31bf812b8ff4f7515d9ef62a
QuynhNhung <dothiquynhnhung12.2@gmail.com>
2024-09-16 23:35:21 +0700

    Login_Register

---
+++ app/src/main/java/com/example/login_register/LoginActivity.java
@@ -0,0 +1,59 @@
+package com.example.login_register;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+    public EditText email;
+    public EditText password;
+    public TextView tVName;
+    public TextView tVMSV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TextView btn = findViewById(R.id.textViewSignUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        email = (EditText) findViewById(R.id.ipEnterEmail);
+        password = (EditText) findViewById(R.id.ipPass);
+        tVName = (TextView) findViewById(R.id.textViewName);
+        tVMSV = (TextView) findViewById(R.id.textViewMSV);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = email.getText().toString();
+                tVName.setText(name);
+
+                String pass = password.getText().toString();
+                tVMSV.setText(pass);
+            }
+        });
+
+
+    }
+}
